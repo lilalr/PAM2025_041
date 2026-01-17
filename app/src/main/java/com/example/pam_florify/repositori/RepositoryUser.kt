f80d23dfad6cb0a7aff426b4b0752b0e12b7ca75
lilalr <gurennut@gmail.com>
2026-01-17 14:37:20 +0700

    buat fitur repositori user dengan logging untuk debug

--- app/src/main/java/com/example/pam_florify/repositori/RepositoryUser.kt
+++ app/src/main/java/com/example/pam_florify/repositori/RepositoryUser.kt
@@ -7,14 +7,17 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 
 interface RepositoryUser {
-    suspend fun login(loginData: Map<String, String>): User?
-    suspend fun register(user: User): Response<ResponseBody>
+    suspend fun login(
+        loginData: Map<String, String>): User?
+    suspend fun register(
+        user: User): Response<ResponseBody>
 }
 
 class NetworkRepositoryUser(
     private val apiService: TanamanApiService
 ) : RepositoryUser {
-    override suspend fun login(loginData: Map<String, String>): User? {
+    override suspend fun login(
+        loginData: Map<String, String>): User? {
         Log.d("NetworkRepository", "Sending login request: $loginData")
         val response = apiService.login(loginData)
         Log.d("NetworkRepository", "Login successful - User: ${response.username}, ID: ${response.userId}")
